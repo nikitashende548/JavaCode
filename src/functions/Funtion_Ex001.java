d694e35c519c5f40bbd00b7addff06e946e0bc61
nikita <nikitashende548@gmail.com>
2025-03-24 00:30:26 +0530

    24March

---
+++ src/functions/Funtion_Ex001.java
@@ -0,0 +1,12 @@
+package functions;
+
+public class Funtion_Ex001
+{
+    public static void main(String[] args)
+    {
+        int max= Math.max(3,4);
+        int min=Math.min(4,6);
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
---
+++ src/functions/MethodFuction_Ex001.java
@@ -0,0 +1,19 @@
+package functions;
+
+public class MethodFuction_Ex001 {
+    static void f1()
+    {
+        System.out.println("hii");
+
+    }
+    static void f2()
+    {
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        f1();
+        f2();
+    }
+}
+
